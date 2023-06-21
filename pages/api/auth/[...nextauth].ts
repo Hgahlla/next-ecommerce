f791c7d5945b90e21be567565f9e4e75135d8083
Hgahlla <harmohangahlla@gmail.com>
2023-06-21 01:40:04 -0500

    Integrated stripe and added stripeCustomerId to User table

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,12 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import Stripe from "stripe";
 
 const prisma = new PrismaClient();
 
-export default NextAuth({
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -14,4 +15,27 @@ export default NextAuth({
     }),
     // Add another provider
   ],
-});
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      });
+
+      if (user.name && user.email) {
+        //Let's create a stripe customer
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+
+        //Also update our prisma user with the stripeCustomerId
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: customer.id },
+        });
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
