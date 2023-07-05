d1a7dcbac78213e4c93ddc2f5f3671757f218bfc
Hgahlla <harmohangahlla@gmail.com>
2023-07-05 13:35:58 -0500

    Formatted total price of the cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -9,6 +9,11 @@ import basket from "@/public/basket.png";
 export default function Cart() {
   const cartStore = useCartStore();
 
+  //Total Price
+  const totalPrice = cartStore.cart.reduce((acc, item) => {
+    return acc + item.unit_amount! * item.quantity!;
+  }, 0);
+
   return (
     <div
       onClick={() => cartStore.toggleCart()}
@@ -71,6 +76,9 @@ export default function Cart() {
             </div>
           </div>
         ))}
+
+        {/* Checkout and total */}
+        {cartStore.cart.length > 0 && <p>Total: {formatPrice(totalPrice)}</p>}
         {cartStore.cart.length > 0 && (
           <button className="mt-4 w-full rounded-md bg-teal-700 py-2 text-white">
             Checkout
